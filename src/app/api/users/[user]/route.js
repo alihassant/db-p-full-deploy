b53ade043247e4f3c9de4386720f6bb8c3773ef2
Ali Hassan <62383138+alihassant@users.noreply.github.com>
2024-02-02 05:21:11 +0500

    some changes

--- src/app/api/users/[user]/route.js
+++ src/app/api/users/[user]/route.js
@@ -5,7 +5,7 @@ import { NextResponse } from "next/server";
 export async function GET(request) {
   const userId = await getDataFromToken(request);
   //   console.log(userId);
-  const res = await axios.get(`https://good-puce-elephant-tie.cyclic.app/auth/user/${userId}`);
+  const res = await axios.get(`https://good-puce-elephant-tie.cyclic.app/api/auth/user/${userId}`);
   const user = res.data;
   //   console.log(res.data);
   return NextResponse.json(user);
