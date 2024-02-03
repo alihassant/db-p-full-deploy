5ce2d4dac96f310bc3b61a969438cbea3d1ef629
Ali Hassan <layanthebot@gmail.com>
2024-02-03 07:01:29 +0500

    api url

--- src/app/api/table/tableData/[id]/route.js
+++ src/app/api/table/tableData/[id]/route.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default async function getData(path) {
   const response = await axios.get(
-    `http://localhost:8080/api/user/getDatabase/${path}`
+    `https://good-puce-elephant-tie.cyclic.app/api/user/getDatabase/${path}`
   );
   return response.data;
   // res.status(200).json(data);
